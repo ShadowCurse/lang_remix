76a26e0edd67fb38e70b018161ad9e253c1a6355
ShadowCurse <shadowcurse57@gmail.com>
2022-06-01 18:37:05 +0300

    added fuzzbuzz in zig

---
+++ fuzzbuzz_zig/README.md
@@ -0,0 +1,10 @@
+# Fuzzbuzz
+
+Simple fuzzbuzz implementation in zig
+
+## Build
+```bash
+$ git clone https://github.com/ShadowCurse/lang_remix/fuzzbuzz_zig
+$ cd fuzzbuzz_zig
+$ zig build
+```
---
+++ fuzzbuzz_zig/src/main.zig
@@ -0,0 +1,92 @@
+const std = @import("std");
+const stdout = std.io.getStdOut().writer();
+const allocator = std.testing.allocator;
+
+fn print_usage() !void {
+    try stdout.print("usage: fuzzbuzz <number> \n<number> - amount numbers to process\n", .{});
+}
+
+fn get_args_len() usize {
+    var args_iter = std.process.args();
+    var argc: usize = 0;
+    while (args_iter.skip()) {
+        argc += 1;
+    }
+    return argc;
+}
+
+const ParseError = error{
+    InvalidChar,
+    Overflow,
+};
+
+fn char_to_digit(char: u8) u8 {
+    return switch (char) {
+        '0'...'9' => char - '0',
+        'a'...'z' => char - 'a' + 10,
+        'A'...'Z' => char - 'A' + 10,
+        else => unreachable,
+    };
+}
+
+fn str_to_number(comptime T: type, str: []const u8, radix: u8) ParseError!T {
+    var number: T = 0;
+    for (str) |c| {
+        const digit = char_to_digit(c);
+
+        if (digit > radix) {
+            return ParseError.InvalidChar;
+        }
+
+        if (@mulWithOverflow(T, number, radix, &number)) {
+            return ParseError.Overflow;
+        }
+
+        if (@addWithOverflow(T, number, digit, &number)) {
+            return ParseError.Overflow;
+        }
+    }
+
+    return number;
+}
+
+fn fuzzbuzz(n: u32) !void {
+    var curr: u32 = 1;
+    while (curr <= n) : (curr += 1) {
+        if (curr % 15 == 0) {
+            try stdout.print("{}: FuzzBuzz\n", .{curr});
+        } else if (curr % 3 == 0) {
+            try stdout.print("{}: Fuzz\n", .{curr});
+        } else if (curr % 5 == 0) {
+            try stdout.print("{}: Buzz\n", .{curr});
+        } else {
+            try stdout.print("{}: {}\n", .{ curr, curr });
+        }
+    }
+}
+
+pub fn main() !void {
+    const argc = get_args_len();
+    if (argc != 2) {
+        try print_usage();
+        std.process.exit(1);
+    }
+
+    var args_iter = std.process.args();
+    const arg_num = 1;
+    var curr_arg: u8 = 0;
+    while (curr_arg < arg_num and args_iter.skip()) : (curr_arg += 1) {}
+    const arg = try args_iter.next(allocator).?;
+    defer allocator.free(arg);
+
+    const n = str_to_number(u32, arg, 10) catch {
+        try print_usage();
+        std.process.exit(1);
+    };
+    try fuzzbuzz(n);
+}
+
+test "parse u32" {
+    const res = try str_to_number(u32, "1234", 10);
+    try std.testing.expect(res == 1234);
+}
