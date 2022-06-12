15b2c7d85a87fed31afa7c89b5fb877ac91399df
ShadowCurse <shadowcurse57@gmail.com>
2022-06-12 16:46:36 +0300

    added fizzbuzz in haskell

---
+++ fizzbuzz_haskell/Main.hs
@@ -0,0 +1,33 @@
+import Data.List
+import Numeric.Natural
+import System.Environment
+import System.Exit
+import Text.Printf
+import Text.Read (readMaybe)
+
+printHelpAndExit :: String
+printHelpAndExit = do
+  "usage: fizzbuzz <number> \n<number> - amount of numbers to evaluate"
+
+parseInteger :: String -> Maybe Natural
+parseInteger [] = Nothing
+parseInteger s = readMaybe . head . words $ s
+
+fizzbuzz :: Natural -> String
+fizzbuzz n = intercalate "\n" [checkNumber x | x <- [1 .. n]]
+
+checkNumber :: Natural -> String
+checkNumber x
+  | x `mod` 15 == 0 = printf "%d: %s" x "Fuzzbuzz"
+  | x `mod` 5 == 0 = printf "%d: %s" x "Fuzz"
+  | x `mod` 3 == 0 = printf "%d: %s" x "Buzz"
+  | otherwise = printf "%d: %d" x x
+
+main :: IO ()
+main = do
+  args <- getArgs
+  if length args /= 1
+    then do
+      putStrLn printHelpAndExit
+      exitSuccess
+    else putStrLn $ maybe printHelpAndExit fizzbuzz (parseInteger $ head args)
---
+++ fizzbuzz_haskell/README.md
@@ -0,0 +1,10 @@
+# Fizzbuzz
+
+Simple fizzbuzz implementation in Haskell
+
+## Build
+```bash
+$ git clone https://github.com/ShadowCurse/lang_remix.git
+$ cd fizzbuzz_haskell
+$ ghc Main.hs 
+```
